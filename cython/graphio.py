865320713ed1ef0361fd91868be7dcf822aad665
Henning Meyerhenke <meyerhenke@kit.edu>
2013-11-14 14:46:59 +0100

    MERGE

---
+++ cython/graphio.py
@@ -0,0 +1,61 @@
+from _NetworKit import (METISGraphReader, FastMETISGraphReader, DotGraphWriter, EdgeListIO, \
+						 LineFileReader, SNAPGraphWriter, ClusteringReader, ClusteringWriter)
+
+import os
+
+def readGraph(path, format=None, fast=False, separator='\t', firstNode=1):
+	"""    Read graph and return a NetworKit::Graph"""
+	# TODO: detect file format by looking at the file content
+	if format is None:    # look at file extension
+		if path.endswith(".graph") or path.endswith(".metis") or path.endswith(".dimacs"):
+			if fast:
+				print("using experimental fast reader")
+				reader = FastMETISGraphReader()
+			else:
+				reader = METISGraphReader()
+		else:
+			raise Exception("unknown graph file format")
+	else:
+		if (format is "metis"):
+			if fast:
+				print("using experimental fast reader")
+				reader = FastMETISGraphReader()
+			else:
+				reader = METISGraphReader()
+		elif (format is "edgelist"):
+			reader = EdgeListIO(separator, firstNode)
+
+	if ("~" in path):
+		path = os.path.expanduser(path)
+		print("path expanded to: {0}".format(path))
+	if not os.path.isfile(path):
+		raise IOError("{0} is not a file".format(path))
+	else:
+		with open(path, "r") as file:    # catch a wrong path before it crashes the interpreter
+			G = reader.read(path)
+			return G
+
+	return None
+
+class GraphConverter:
+	
+	def __init__(self, reader, writer):
+		self.reader = reader
+		self.writer = writer
+		
+	def convert(self, inPath, outPath):
+		G = self.reader.read(inPath)
+		self.writer.write(G, outPath)
+		
+	def __str__(self):
+		return "GraphConverter: {0} => {0}".format(self.reader, self.writer)
+
+def getConverter(fromFormat, toFormat):
+	
+	readers = {"metis": METISGraphReader, "edgelist" : EdgeListIO}    
+	writers = {"edgelist": EdgeListIO}
+	
+	reader = readers[fromFormat]()
+	writer = writers[toFormat]()
+	
+	return GraphConverter(reader, writer)
