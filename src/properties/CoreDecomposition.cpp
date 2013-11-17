2cc06d089f52892a2a0dc02acb07d6c86d7c73a0
gbrueckner <gbrueckner@localhost>
2013-11-17 04:58:51 +0100

    Added solutions to exercises 3 and 6.

---
+++ src/properties/CoreDecomposition.cpp
@@ -0,0 +1,57 @@
+/*
+ * CoreDecomposition.cpp
+ *
+ *  Created on: Oct 28, 2013
+ *      Author: Henning
+ */
+
+#include "CoreDecomposition.h"
+
+namespace NetworKit {
+
+CoreDecomposition::CoreDecomposition() {
+
+}
+
+CoreDecomposition::~CoreDecomposition() {
+
+}
+
+std::vector<count> CoreDecomposition::run(const Graph& G) {
+
+    using namespace std;
+
+	vector<count> degree(G.numberOfNodes());
+    vector<list<node>> nodeLists(G.numberOfNodes(), list<node>());
+    vector<pair<list<node>, list<node>::iterator>> nodeLocation(G.numberOfNodes());
+
+    G.forNodes([&](node v) {
+        degree[v] = G.degree(v);
+        nodeLists[degree[v]].push_front(v);
+        nodeLocation[v] = pair<list<node>, list<node>::iterator>(nodeLists[degree[v]], nodeLists[degree[v]].begin());
+    });
+
+    vector<count> coreness(G.numberOfNodes());
+    Graph G2 = G;
+    index i = 0;
+
+    while (G2.numberOfNodes() > 0) {
+        for (node u : nodeLists[i]) {
+            coreness[u] = i;
+            G2.forNeighborsOf(u, [&](node v) {
+                if (--degree[v] >= i) {
+                    nodeLocation[v].first.erase(nodeLocation[v].second);
+                    nodeLists[degree[v]].push_back(v);
+                    nodeLocation[v] = pair<list<node>, list<node>::iterator>(nodeLists[degree[v]], prev(nodeLists[degree[v]].end()));
+                }
+                G2.removeEdge(u, v);
+            });
+            G2.removeNode(u);
+        }
+        i++;
+    }
+
+    return coreness;
+}
+
+} /* namespace NetworKit */
